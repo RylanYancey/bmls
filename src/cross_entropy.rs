579e427a2c5f117f6bc2b00d23af99923a1bcd1c
Rylan Yancey <rylanyancey@gmail.com>
2023-11-15 12:31:53 -0500

    Added Error Handling to Cross Entropy

--- src/cross_entropy.rs
+++ src/cross_entropy.rs
@@ -1,5 +1,6 @@
 
-// negative sum of y * ln(p)
+use crate::error::BMLSError;
+use crate::error;
 
 #[inline]
 pub fn cross_entropy(
@@ -8,7 +9,24 @@ pub fn cross_entropy(
     e: &mut [f32],
     g: &mut [f32],
     dim: [usize; 2],
-) {
+) -> Result<(), BMLSError> {
+    let len = dim[0] * dim[1];
+    if t.len() != len {
+        return error::length_mismatch("T", t.len(), "Dim", len)
+    }
+
+    if t.len() != p.len() {
+        return error::length_mismatch("T", t.len(), "P", p.len())
+    }
+
+    if e.len() != dim[0] {
+        return error::length_mismatch("E", e.len(), "Dim[0]", dim[0])
+    }
+
+    if g.len() != p.len() {
+        return error::length_mismatch("G", g.len(), "P", p.len())
+    }
+
     let rows = dim[0];
     let cols = dim[1];
 
@@ -22,4 +40,6 @@ pub fn cross_entropy(
         }
         e[i] = -sum;
     }
+
+    Ok(())
 }
